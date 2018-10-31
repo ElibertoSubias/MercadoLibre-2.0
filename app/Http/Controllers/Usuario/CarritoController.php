22f3b2f8c9cd6582f980c5c84470e745ee21cecd
erick6672 <erick6672@gmail.com>
2018-10-30 22:42:43 -0700

    carrito con ajax

--- app/Http/Controllers/Usuario/CarritoController.php
+++ app/Http/Controllers/Usuario/CarritoController.php
@@ -23,7 +23,7 @@ class CarritoController extends Controller
         $aux = Articulos::where('_id' , '=', $id->idPublicacion)->get();
     	array_push($articulo, $aux[0]);
     	   
-           $i=$id->precio + $i;	
+           $i=$id->precio + $i;
            
     	}
         
@@ -58,7 +58,7 @@ class CarritoController extends Controller
             return response()->json([
                     "articulo" => $articulo,
                     "totalArticulos" => $totalArticulos,
-                    "tipoMensaje" => "Ajax"
+                    "subtotal" => $i
                 ]);
         }
 
