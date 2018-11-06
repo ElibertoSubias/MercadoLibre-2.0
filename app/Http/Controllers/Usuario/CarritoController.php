8767b5eaa9ea02ce514e35bac5b549088dd1482f
ElibertoSubias <eliberto.subias.ortega@gmail.com>
2018-11-05 19:58:45 -0700

    correcion agregadocarrito - agregadoCarrito

--- app/Http/Controllers/Usuario/CarritoController.php
+++ app/Http/Controllers/Usuario/CarritoController.php
@@ -61,7 +61,7 @@ class CarritoController extends Controller
             $Carrito->precio = $request->precio;        
             $Carrito->save();
             $urlImagen = $request->idUser."/".$request->idPublicacion; 
-            return view('usuario.carrito.agregadocarrito')->with(['precio'=>$request->precio,'titulo'=>$request->titulo,'urlImagen'=>$urlImagen]);
+            return view('usuario.carrito.agregadoCarrito')->with(['precio'=>$request->precio,'titulo'=>$request->titulo,'urlImagen'=>$urlImagen]);
 
             }
             else
@@ -72,7 +72,7 @@ class CarritoController extends Controller
                 $nprecio=($total * $aux[0]->precio); 
                 $datos = Carrito::where([['idUser' , '=', auth()->user()->id] ,['idPublicacion', '=',  $request->idArticulo]])->update(['cantidad' => $total, 'precio' => $nprecio]);
                 $urlImagen = $request->idUser."/".$request->idPublicacion; 
-                return view('usuario.carrito.agregadocarrito')->with(['precio'=>$request->precio,'titulo'=>$request->titulo,'urlImagen'=>$urlImagen]);
+                return view('usuario.carrito.agregadoCarrito')->with(['precio'=>$request->precio,'titulo'=>$request->titulo,'urlImagen'=>$urlImagen]);
         }
         // $Carrito = new Carrito;
         // $Carrito->idUser = auth()->user()->id;
