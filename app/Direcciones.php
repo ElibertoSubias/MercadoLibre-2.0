0424c0367471ef1f6fb3f115562c52c5afafe5ba
erick6672 <erick6672@gmail.com>
2018-11-08 07:37:14 -0700

    coleccion carritos

---
+++ app/Direcciones.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;  
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Direcciones extends Eloquent
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'direcciones'; 
+    protected $primarykey = 'id';
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'calle', 'numeroEx', 's/n' ,'numeroInt', 'entrecalles', 'referencia', 'codigopostal', 'asentamiento', 'municipio', 'estado'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'remember_token',
+    ];
+}
