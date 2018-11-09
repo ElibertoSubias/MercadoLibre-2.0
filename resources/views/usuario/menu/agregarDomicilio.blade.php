dc281f8de4eeae8690cca4df0d91cfc1ac817e03
ElibertoSubias <eliberto.subias.ortega@gmail.com>
2018-11-08 17:32:10 -0700

    Acomodo de Botones de Agregar Domicilio

--- resources/views/usuario/menu/agregarDomicilio.blade.php
+++ resources/views/usuario/menu/agregarDomicilio.blade.php
@@ -17,13 +17,13 @@
 
 
 
-<form class="ui-form form-with-floating-labels" id="addresses-form" action="" name="addresses-form" method="post">
+<form style="margin-bottom:0px;" class="ui-form form-with-floating-labels" id="addresses-form" action="" name="addresses-form" method="post">
     <input type="hidden" name="latitude" id="latitude" value=""><input type="hidden" name="longitude" id="longitude" value=""><input type="hidden" name="locationType" id="locationType" value=""><input type="hidden" name="formId" id="formId" value="ce86e2344591f29a66b866c76d924d3419662143ae6feab741d29f2bd078dd6a"><input type="hidden" name="appName" id="appName" value="generic">
     
     
     
         <div class="ml-addresses-card">
-            
+            <p>Datos del domicilio</p>
                  <fieldset class="ml-addresses-fieldset">
     <legend class="ml-addresses-hidden">Dirección</legend>
     <div class="u-clearfix">
@@ -37,14 +37,19 @@
         </div>
         <div class="ml-addresses-grid ml-addresses-grid--second-column ml-addresses-grid--half-column">
             <div class="ui-form__row ">
-                <input id="inputEmail" type="tel" data-required="true" class="ui-form__input " name="numExterior" value="" data-valid="true" aria-describedby="numberMessage" maxlength="8" data-min-length="1">
-                <label id="lblMsjinputEmail" for="number" class="ui-form__label">Nº Ext.</label>
+                <input id="inputNumExt" type="tel" data-required="true" class="ui-form__input " name="inputNumExt" value="" data-valid="true" aria-describedby="numberMessage" maxlength="8" data-min-length="1">
+                <label id="lblMsjinputNumExt" for="inputNumExt" class="ui-form__label">Nº Ext.</label>
                 <span class="ui-form__status-bar"></span>
-                <div class="ui-form__message" id="alertMsjinputEmail" role="alert">error_place_holder</div>
+                <div class="ui-form__message" id="alertMsjinputNumExt" role="alert">error_place_holder</div>
             </div>
+
             <div class="ml-addresses-embeded-action">
-                <label class="ui-checkbox ui-checkbox--embedded ui-checkbox--label-after">
-                    <span class="ui-checkbox__label">s/n</span>
+                <label class="ui-checkbox ui-checkbox--embedded ui-checkbox--label-after" style="    position: relative;
+    top: 35px;left: -10px;
+    width: 25px;
+    height: 35px;">
+                    <span class="ui-checkbox__label" style="left: 5px;
+    position: absolute;">s/n</span>
                     <input class="ui-checkbox__input" type="checkbox" id="sn" name="address.sn">
                     <span class="ui-checkbox__mimic"></span>
                     <div class="ui-form__message" id="numberMessage" role="alert">error_place_holder</div>
@@ -53,30 +58,30 @@
         </div>
         <div class="ml-addresses-grid ml-addresses-grid--second-column ml-addresses-grid--half-column">
             <div class="ui-form__row ">
-                <input id="info" type="text" class="ui-form__input " name="address.comment" value="" aria-describedby="commentMessage" data-valid="true" maxlength="150" data-required="false" data-min-length="-1">
+                <input id="inputNumInt" type="text" class="ui-form__input " name="inputNumInt" value="" aria-describedby="commentMessage" data-valid="true" maxlength="150" data-required="false" data-min-length="-1">
 
-                <label for="info" class="ui-form__label">Nº Int. (opcional)</label>
+                <label id="lblMsjinputNumInt" for="inputNumInt" class="ui-form__label">Nº Int. (opcional)</label>
                 <span class="ui-form__status-bar"></span>
-                <div class="ui-form__message" id="numberMessage" role="alert">error_place_holder</div>
+                <div class="ui-form__message" id="alertMsjinputNumInt" role="alert">error_place_holder</div>
             </div>
         </div>
     </div>
     <div class="u-clearfix">
         <div class="ml-addresses-grid ml-addresses-grid--second-column">
             <div class="ui-form__row ">
-                <input id="references" type="text" class="ui-form__input " name="address.references" value="" aria-describedby="referencesMessage" data-valid="true" maxlength="150" data-required="false" data-min-length="-1">
-                <label for="references" class="ui-form__label">Entre calles (opcional)</label>
+                <input id="inputEntreCalles" type="text" class="ui-form__input " name="inputEntreCalles" value="" aria-describedby="referencesMessage" data-valid="true" maxlength="150" data-required="false" data-min-length="-1">
+                <label id="lblMsjinputEntreCalles" for="inputEntreCalles" class="ui-form__label">Entre calles (opcional)</label>
                 <span class="ui-form__status-bar"></span>
-                <div class="ui-form__message" id="numberMessage" role="alert">error_place_holder</div>
+                <div class="ui-form__message" id="alertMsjinputEntreCalles" role="alert">error_place_holder</div>
             </div>
         </div>
     </div>
     <div class="ml-addresses-grid ml-addresses-grid--full-column">
         <div class="ui-form__row ">
-            <input id="extraReferences" type="text" class="ui-form__input " name="address.extraReferences" value="" aria-describedby="extraReferencesMessage" data-valid="true" data-required="true" maxlength="150" data-min-length="3">
-            <label for="extraReferences" class="ui-form__label">Referencias</label>
+            <input id="inputReferencias" type="text" class="ui-form__input " name="inputReferencias" value="" aria-describedby="extraReferencesMessage" data-valid="true" data-required="true" maxlength="150" data-min-length="3">
+            <label id="lblMsjinputReferencias" for="inputReferencias" class="ui-form__label">Referencias</label>
             <span class="ui-form__status-bar"></span>
-            <div class="ui-form__message" id="numberMessage" role="alert">error_place_holder</div>
+            <div class="ui-form__message" id="alertMsjinputReferencias" role="alert">error_place_holder</div>
         </div>
     </div>
 </fieldset>
@@ -87,10 +92,10 @@
     <legend class="ml-addresses-hidden">Zipcode</legend>
     <div class="ml-addresses-grid ml-addresses-grid--first-column">
         <div class="ui-form__row ">
-            <input id="zipCode" type="tel" data-required="true" class="ui-form__input " name="address.zip_code" value="" aria-describedby="zipcodeMessage" data-valid="true" maxlength="5" data-min-length="5">
-            <label for="zipCode" class="ui-form__label">Código postal</label>
+            <input id="inputCodigoPostal" type="tel" data-required="true" class="ui-form__input " name="inputCodigoPostal" value="" aria-describedby="zipcodeMessage" data-valid="true" maxlength="5" data-min-length="5">
+            <label id="lblMsjinputCodigoPostal" for="inputCodigoPostal" class="ui-form__label">Código postal</label>
             <span class="ui-form__status-bar"></span>
-            <div class="ui-form__message" id="numberMessage" role="alert">error_place_holder</div>
+            <div class="ui-form__message" id="alertMsjinputCodigoPostal" role="alert">error_place_holder</div>
         </div>
         <div class="ml-addresses-embeded-action">
             
@@ -113,31 +118,29 @@
     <fieldset id="neighborhoodField" class="ml-addresses-fieldset">
         <div class="ml-addresses-grid ml-addresses-grid--first-column">
             <div class="ui-form__row ">
-                <input id="neighborhood" name="address.neighborhood.name" type="text" data-required="true" class="ui-form__input " autocomplete="off" value="" aria-describedby="neighborhoodMessage" data-valid="true" maxlength="50" data-min-length="3">
-                <label for="neighborhood" id="neighborhoodLabel" class="ui-form__label">Asentamiento</label>
+                <input id="inputAcentamiento" name="inputAcentamiento" type="text" data-required="true" class="ui-form__input " autocomplete="off" value="" aria-describedby="neighborhoodMessage" data-valid="true" maxlength="50" data-min-length="3">
+                <label for="inputAcentamiento" id="lblMsjinputAcentamiento" class="ui-form__label">Asentamiento</label>
                 <span class="ui-form__status-bar"></span>
-
+                <div class="ui-form__message" id="alertMsjinputAcentamiento" role="alert">error_place_holder</div>
             </div>
         </div>
     </fieldset>
     <div class="ml-addresses-fieldset__inline-container">
         <div class="ml-addresses-grid ml-addresses-grid--first-column">
-            <div class="ui-form__row ">
-                <input type="hidden" id="cityTemporary" name="address.city.id" value="">
-                <input type="text" id="city" name="address.city.name" value="" disabled="disabled" data-required="true" data-valid="true" class="ui-form__input ">
-                <label for="city" id="cityLabel" class="ui-form__label">Municipio</label>
+            <div class="ui-form__row "> 
+                <input type="text" id="inputMunicipio" name="inputMunicipio" value="" data-required="true" data-valid="true" class="ui-form__input ">
+                <label for="inputMunicipio" id="lblMsjinputMunicipio" class="ui-form__label">Municipio</label>
                 <span class="ui-form__status-bar"></span>
-
+                <div class="ui-form__message" id="alertMsjinputMunicipio" role="alert">error_place_holder</div>
             <div class="ml-addresses-suggest"><ul class="ml-addresses-suggest-list"></ul></div></div>
         </div>
 
         <div class="ml-addresses-grid ml-addresses-grid--second-column">
-            <div class="ui-form__row  ">
-                <input type="hidden" id="stateTemporary" name="address.state.id" value="">
-                <input type="text" id="state" value="" disabled="disabled" data-required="true" data-valid="true" class="ui-form__input ">
-                <label for="state" class="ui-form__label">Estado</label>
+            <div class="ui-form__row  "> 
+                <input type="text" id="inputEstado" value="" data-required="true" name="inputEstado" data-valid="true" class="ui-form__input ">
+                <label id="lblMsjinputEstado" for="inputEstado" class="ui-form__label">Estado</label>
                 <span class="ui-form__status-bar"></span>
-
+                <div class="ui-form__message" id="alertMsjinputEstado" role="alert">error_place_holder</div>
             </div>
         </div>
     </div>
@@ -145,29 +148,38 @@
 
             
         </div>
-    
+    <div class="ml-addresses-card">
+        <p>Datos de contacto</p>
+        <div class="u-clearfix">
+        <div class="ml-addresses-grid ml-addresses-grid--first-column col-md-5">
+            <div class="ui-form__row ">
+                <input id="inputContacto" type="text" data-required="true" class="ui-form__input " name="inputContacto" value="" aria-describedby="contactMessage" data-valid="true" maxlength="150" data-min-length="3">
+                <label id="lblMsjinputContacto" for="inputContacto" class="ui-form__label">Contacto</label>
+                <span class="ui-form__status-bar"></span>
+                <div class="ui-form__message" id="alertMsjinputContacto" role="alert">error_place_holder</div>
+            </div>
+        </div>
+        <div class="ml-addresses-grid ml-addresses-grid--second-column ui-telephone  col-md-5">
+            <div class="ui-telephone__number ui-form__row">
+                    <input id="inputTelefono" class="ui-telephone__input " data-js="telephone" name="inputTelefono" type="tel" value="" maxlength="22" placeholder="">
+                    <label id="lblMsjinputTelefono" for="inputTelefono" class="ui-form__label">Teléfono</label>
+                    <span class="ui-form__status-bar"></span>
+                <span class="ui-telephone__focus-line"></span>
+            </div>
+            <div id="alertMsjinputinputTelefono" class="ui-telephone__validations" data-js="tel-validations"> 
+        </div>
+    </div>
+    </div>
 
     
-        
-    <div class="ml-addresses-actions ml-addresses-actions--align-left">
+ </div>       
+    <div class="ml-addresses-actions ml-addresses-actions--align-left" style="text-align: left;margin-bottom: 0px;padding: 0px 0px 40px;">
     <input id="continueButton" type="submit" class="ui-button ui-button--primary" value="Guardar">
     <a id="cancelButton" href="https://myaccount.mercadolibre.com.mx/profile/" class="ui-button ui-button--tertiary">Cancelar</a>
-</div>
+
 
     
-</form>
-
-    <!-- Hotjar Tracking Code for Addresses -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:452650,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+</form> 
 
 
 
